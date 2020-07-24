b77337a199aa9e741d1ae8ef45f47443465b75db
Rob Howard <rob.howard@cultureamp.com>
2020-07-24 10:59:44 +1000

    Adds the initial version of the gallery plugin.

---
+++ gallery/src/gallery.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+
+# Only ruby stdlib "libraries" here; https://ruby-doc.org/stdlib-2.7.0/ has a list of what's available.
+require 'erb'
+require 'pathname'
+
+class GalleryTemplate < Struct.new(
+  :buildkite_pipeline_name,
+  :gallery_url_base,
+  :canonical_branch_images_dir,
+  :canonical_repo_and_branch,
+  :triggering_commit,
+  :triggering_commit_images_dir,
+  :triggering_repo_and_branch,
+  keyword_init: true,
+)
+  Image = Struct.new(:url, :filename, :title, keyword_init: true)
+
+  def render
+    erb = File.read(File.join(__dir__, 'gallery.html.erb'))
+    ERB.new(erb).result(binding)
+  end
+
+  private
+
+  def triggering_commit_images
+    @triggering_commit_images ||= images_for_path(triggering_commit_images_dir)
+  end
+
+  def canonical_branch_images
+    @canonical_branch_images ||= images_for_path(canonical_branch_images_dir)
+  end
+
+  def gallery_images
+    return @gallery_images if @gallery_images
+
+    # Make a hash of { filename => Image }
+    canonical_by_name =
+      canonical_branch_images.map { |image| [ image.filename, image ] }.to_h
+
+    # Make an array of pairs of Commit and Comparison images.
+    @gallery_images = triggering_commit_images.map do |triggering_commit_image|
+      [ triggering_commit_image, canonical_by_name.delete(triggering_commit_image.filename) ]
+    end
+  end
+
+  def has_canonical_branch?
+    !canonical_branch_images.empty?
+  end
+
+  ### Helpers
+
+  def images_for_path(path)
+    Dir[File.join(path, '*.png')].map do |filename|
+      Image.new(
+        url: "#{gallery_url_base}#{nice_path(filename)}",
+        filename: File.basename(filename),
+        title: File.basename(filename, '.png'),
+      )
+    end
+  end
+
+  # "./foo/bar" => "foo/bar"
+  def nice_path(relative_path)
+    Pathname
+      .new(File.expand_path(relative_path))
+      .relative_path_from(Dir.pwd)
+      .to_s
+  end
+end
+
+env_vars = %w(
+  buildkite_pipeline_name
+  gallery_url_base
+  canonical_branch_images_dir
+  canonical_repo_and_branch
+  triggering_commit
+  triggering_commit_images_dir
+  triggering_repo_and_branch
+)
+template = GalleryTemplate.new(
+  env_vars.map {|var| [var.to_sym, ENV.fetch(var)] }.to_h
+)
+File.write(ARGV.fetch(0), template.render)
---
+++ gallery/src/github_status.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+# Are we running as a script that has been 'source'd, eg. 'source ./bin/support/github_status.sh'?
+(return 0 2>/dev/null) && IS_SOURCED_SCRIPT=true || IS_SOURCED_SCRIPT=false
+
+# Stub out the notification functions; it's a no-op by default, and only 'activated'
+# in the appropriate context (in CI).
+post_status_to_current_github_pr(){ true; }
+post_status_to_triggering_github_pr(){ true; }
+
+if [ -z "${BUILDKITE:-}" ]; then
+  # Not running in CI; either exit completely, or return to the script that's 'source'-ing us.
+  "$IS_SOURCED_SCRIPT" && return 0 || exit 0
+fi
+
+# ------------------------------------------------------------------
+
+# Private helper used by the post_status_to_...() functions below.
+get_github_api_token() {
+  # Not set in the environment...?
+  if [ -z "${GITHUB_STATUS_ACCESS_TOKEN:-}" ]; then
+    # ... Then get GITHUB_STATUS_ACCESS_TOKEN from parameter store.
+
+    if [ -z "${GITHUB_STATUS_ACCESS_TOKEN_PARAM_PATH:-}" ] || ! GITHUB_STATUS_ACCESS_TOKEN="$(
+      aws ssm get-parameter --name "${GITHUB_STATUS_ACCESS_TOKEN_PARAM_PATH}" \
+        --with-decryption --query=Parameter.Value --output=text
+    )"; then
+      >&2 echo \
+        "A GITHUB_STATUS_ACCESS_TOKEN is required to post status to github." \
+        "For local development this can be set as an environment variable." \
+        "In the CI account this is pulled from paramater store." \
+        "Please set the value of GITHUB_STATUS_ACCESS_TOKEN, or pass a" \
+        "GITHUB_STATUS_ACCESS_TOKEN_PARAM_PATH (eg. '/visual-tests/GITHUB_STATUS_ACCESS_TOKEN')" \
+        "and ensure your CI step has access to pull ${GITHUB_STATUS_ACCESS_PARAM:-that parameter}" \
+        "from parameter store."
+        return 1
+    fi
+  fi
+  echo "$GITHUB_STATUS_ACCESS_TOKEN"
+}
+
+# Private helper used by the post_status_to_...() functions below.
+post_status() {
+  local status=$1 # success, pending, failure
+  local target_url=$2
+  local description=$3
+  local sha_url=$4
+
+  local github_api_token
+  if ! github_api_token=$(get_github_api_token); then
+    return 0 # Silently fail if no token has been found.
+  fi
+
+  local context="System Tests"
+  local time_since_start="$((SECONDS / 60)) minutes, $((SECONDS % 60)) seconds" # Uses Bash's $SECONDS global.
+  case "$status" in
+    pending)
+      : "${description:="Build #${BUILDKITE_BUILD_NUMBER} started"}"
+      ;;
+    success)
+      : "${description:="Build #${BUILDKITE_BUILD_NUMBER} passed (${time_since_start})"}"
+      ;;
+    failure)
+      : "${description:="Build #${BUILDKITE_BUILD_NUMBER} failed (${time_since_start})"}"
+      ;;
+    *)
+      echo "Invalid GitHub status: '${status}' (Must be: success, pending or failure)"
+      return 1
+      ;;
+  esac
+
+  curl --fail --silent -request POST --url "$sha_url" \
+    --header "authorization: Bearer ${github_api_token}" \
+    --header 'content-type: application/json' \
+    --data "$(
+       echo '{}' | jq \
+         --arg state "$status" \
+         --arg description "$description" \
+         --arg context "$context" \
+         --arg target_url "$target_url" \
+         ' .
+         | .["state"]=$state
+         | .["description"]=$description
+         | .["context"]=$context
+         | .["target_url"]=$target_url' \
+    )"
+}
+
+post_status_to_current_github_pr() {
+  local status=$1 # success, pending, failure
+  local target_url=$2
+  local description=${3:-}
+
+  # The GitHub API URL we hit to tell GH about the status of the build.
+  local sha_url="https://api.github.com/repos/${BUILDKITE_GITHUB_ORG}/${BUILDKITE_GITHUB_REPO}/statuses/${BUILDKITE_COMMIT}"
+
+  echo "--- Setting GitHub status"
+  echo "Setting the status '$status' and description '$description' on commit SHA $BUILDKITE_COMMIT with target URL: $target_url"
+
+  post_status "$status" "$target_url" "$description" "$sha_url"
+}
+
+post_status_to_triggering_github_pr() {
+  local status=$1 # success, pending, failure
+  local target_url=$2
+  local description=${3:-}
+
+  if [ -z "${BUILDKITE_TRIGGERED_FROM_GITHUB_ORG:-}" ]; then
+    return 0 # Not a triggered build.
+  fi
+
+  # The GitHub API URL we hit to tell GH about the status of the build.
+  local sha_url="https://api.github.com/repos/${BUILDKITE_TRIGGERED_FROM_GITHUB_ORG}/${BUILDKITE_TRIGGERED_FROM_GITHUB_REPO}/statuses/${BUILDKITE_TRIGGERED_FROM_COMMIT}"
+
+  echo "--- Setting GitHub status on triggering build"
+  echo "Setting the status '$status' and description '$description' on commit SHA $BUILDKITE_TRIGGERED_FROM_COMMIT with target URL: $target_url"
+
+  post_status "$status" "$target_url" "$description" "$sha_url"
+}
